39c2576ce8969791fbf9e6df366ddb0f1603929b
Arnav Nagpurkar <arnav.nagpurkar@gmail.com>
2023-12-31 15:48:21 +0530

    Password Validator

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Password Validator
+
+This project checks whether the given password is valid or not.<br>
+If not then the project will suggest ways to make it stronger.<br>
+If it is valid then it will tell you that the password is valid!<br>
+
+## Hosted on [GitHub Pages](https://arnavnagpurkar.github.io/Password-Validator/) and [Replit](https://password-validator.arnavnagpurkar.repl.co)
+
+## Links to access the project:
+- [https://arnavnagpurkar.github.io/Password-Validator/](https://arnavnagpurkar.github.io/Password-Validator/)
+- [https://password-validator.arnavnagpurkar.repl.co](https://password-validator.arnavnagpurkar.repl.co/)
---
+++ script.js
@@ -0,0 +1,106 @@
+let btn = document.querySelector("button");
+let pass_status = document.createElement('div');
+pass_status.classList.add('status');
+
+document.body.appendChild(pass_status);
+
+function generateUpperCaseLetters() {
+    let letters = [];
+    for (let i = 65; i <= 90; i++) {
+        letters.push(String.fromCharCode(i));
+    }
+    return letters;
+}
+
+const upperCase = generateUpperCaseLetters();
+
+function generateLowercaseLetters() {
+    let letters = [];
+    for (let i = 97; i <= 122; i++) {
+        letters.push(String.fromCharCode(i));
+    }
+    return letters;
+}
+
+const lowerCase = generateLowercaseLetters();
+
+function generateSymbols() {
+    let symbols = [];
+    for (let i = 33; i <= 47; i++) {
+        symbols.push(String.fromCharCode(i));
+    }
+    for (let i = 58; i <= 64; i++) {
+        symbols.push(String.fromCharCode(i));
+    }
+    for (let i = 91; i <= 96; i++) {
+        symbols.push(String.fromCharCode(i));
+    }
+    for (let i = 123; i <= 126; i++) {
+        symbols.push(String.fromCharCode(i));
+    }
+    return symbols;
+}
+
+const symbols = generateSymbols();
+
+const hasUpperCase = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (upperCase.includes(pass[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const hasLowerCase = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (lowerCase.includes(pass[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const hasSymbols = (pass) => {
+    for (let i = 0; i < pass.length; i++) {
+        if (symbols.includes(pass[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
+function checkPass (pass) {
+    if (pass) {
+        let valid = true;
+
+        if (pass.length <= 8) {
+            pass_status.innerHTML = "Length of Password should be greater than 8<br>";
+            valid = false;
+        }
+        if (!hasUpperCase(pass)) {
+            pass_status.innerHTML += "Your Password should have at least 1 uppercase character<br>";
+            valid = false;
+        }
+        if (!hasLowerCase(pass)) {
+            pass_status.innerHTML += "Your Password should have at least 1 lowercase character<br>";
+            valid = false;
+        }
+        if (!hasSymbols(pass)) {
+            pass_status.innerHTML += "Your Password should have at least 1 symbol<br>";
+            valid = false;
+        }
+        if (valid) {
+            pass_status.innerHTML += "Your Password is Valid!";
+            pass_status.style.color = 'white';
+        }
+    } 
+    else {
+        pass_status.innerHTML = "Please enter your Password";
+    }
+}
+
+btn.addEventListener("click", () => {
+    let pass = prompt("Enter your Password");
+    checkPass(pass);
+});
